1e3f84f9734cc77704e862211a6438d395d54ade
Nicolas Gutkowski <nickjgski@gmail.com>
2018-04-21 16:16:14 -0400

    Added arduino test file

---
+++ ailerons.ino
@@ -0,0 +1,33 @@
+
+
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+// called this way, it uses the default address 0x40
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+#define SERVOMIN  150 // this is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  600 // this is the 'maximum' pulse length count (out of 4096)
+
+void setup() {
+  Serial.begin(9600);
+
+  pwm.begin();
+  
+  pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
+
+  delay(10);
+}
+
+void loop() {
+
+  uint16_t pulselen = map(0, 0, 180, SERVOMIN, SERVOMAX);
+  pwm.setPWM(0, 0, pulselen);
+  pwm.setPWM(1, 0, pulselen);
+  pwm.setPWM(2, 0, pulselen);
+  pwm.setPWM(3, 0, pulselen);
+  delay(5000);
+
+  
+ 
+}
